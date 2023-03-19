945bfece5f816f6c2b0b7951c979ba4de6c16cf9
Leo Zhang <leotheasianlion@gmail.com>
2023-03-19 19:47:05 -0400

    big update
    app can now print idea contents stored in the remote repository when using the "--view" flag

--- cmdline_tools/dotnet_eureka/Program.cs
+++ cmdline_tools/dotnet_eureka/Program.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.CommandLine.Invocation;
-using System.CommandLine.Binding;
-using DotNetEnv;
+﻿using System.Text;
 using System.CommandLine;
 using System.Diagnostics;
-using System.Text;
-using Newtonsoft.Json;
 using Octokit;
-
-
 
 public class Program
 {
@@ -26,29 +19,85 @@ public class Program
             description: "Decide whether the created repository should be private. Default is false.");
 
         var repoName = new Option<string>(
-              name: "--repoName",
+              name: "--reponame",
               description: "The name of your repository");
 
         var githubUserName = new Option<string>(
               name: "--username",
               description: "Your github username");
 
+        var editorToUse = new Option<string>(
+            name: "--editor",
+            description: "The editor you would like to use. Defaults to your editor set by your $EDITOR environment variable, if it exists, otherwise defaults to code",
+            getDefaultValue: () =>
+            {
+                return Environment.GetEnvironmentVariable("EDITOR") ?? "code";
+            }
+        );
+
+        var fileName = new Option<string>(
+            name: "--filename",
+            description: "name of the file you input your ideas in"
+        );
+
+        var view = new Option<bool>(
+            name: "--view",
+            description: "print out all of your stored ideas"
+        );
+
         var rootCommand = new RootCommand();
         rootCommand.Add(repoPath);
         rootCommand.Add(isPrivateOption);
         rootCommand.Add(repoName);
         rootCommand.Add(githubUserName);
+        rootCommand.Add(editorToUse);
+        rootCommand.Add(fileName);
+        rootCommand.Add(view);
 
-        rootCommand.SetHandler(async (string path, string name, bool isPrivate, string githubUserName) =>
+        rootCommand.SetHandler(async (string repoPath, string ideaRepoName, bool isPrivate, string githubUserName, string editorToUse, string fileName, bool view) =>
             {
-                await DoRootCommand(path, name, isPrivate, githubUserName);
-            }, repoPath, repoName,isPrivateOption, githubUserName
+                if (view)
+                {
+                    await ShowIdeas(ideaRepoName, githubUserName);
+                }
+                else
+                {
+                    await RunEureka(repoPath, ideaRepoName, isPrivate, githubUserName, editorToUse, fileName);
+                }
+            }, repoPath, repoName, isPrivateOption, githubUserName, editorToUse, fileName, view
         );
 
         await rootCommand.InvokeAsync(args);
     }
 
-    public static async Task DoRootCommand(string path, string name, bool isPrivate, string githubUserName)
+     public static async Task ShowIdeas(string ideaRepoName, string githubUserName)
+     {
+        string accessToken = Environment.GetEnvironmentVariable("githubAPIToken")!;
+        var client = new GitHubClient(new ProductHeaderValue("testing"))
+        {
+            Credentials = new Credentials(Environment.GetEnvironmentVariable("githubAPIToken")!)
+        };
+
+        var fileContents = await client.Repository.Content.GetAllContents(githubUserName, ideaRepoName);
+
+        // Print the contents of the file
+        foreach (var fileContent in fileContents)
+        {
+            if (fileContent.Type == ContentType.File)
+            {
+                // Get the contents of the file
+                var stuff = await client.Repository.Content.GetRawContent(githubUserName, ideaRepoName, fileContent.Path);
+
+                // Print the contents of the file
+                Console.WriteLine($"Contents of {fileContent.Path}:");
+
+                var decodedContents = Encoding.UTF8.GetString(stuff);
+                Console.WriteLine(decodedContents);
+            }
+        }
+     }
+
+    public static async Task RunEureka(string repoPath, string ideaRepoName, bool isPrivate, string githubUserName, string editorToUse, string fileName)
     {
         #region initialize parameters
         string accessToken = Environment.GetEnvironmentVariable("githubAPIToken")!;
@@ -56,61 +105,48 @@ public class Program
         {
             Credentials = new Credentials(Environment.GetEnvironmentVariable("githubAPIToken")!)
         };
-        var filename = $"{DateTime.Now.ToString("yyyy_mm_dd_HH_MM_ss")}.txt";
-        var filePath = Path.Combine(path, name);
+        var filename = $"{fileName}_{DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")}.txt";
+        var filePath = Path.Combine(repoPath, ideaRepoName);
         var fullPathToFile = Path.Combine(filePath, filename);
         #endregion
-
-        #region temporary comment for now
-        // var editor = Environment.GetEnvironmentVariable("EDITOR");
-        // if (string.IsNullOrEmpty(editor))
-        // {
-        //     Console.WriteLine("EDITOR environment variable not set.");
-        //     return;
-        // }
-        #endregion
-
 
         #region create local repo and make the link to remote repo
         if (!Directory.Exists(filePath))
         {
             Directory.CreateDirectory(filePath);
         }
-
         Directory.SetCurrentDirectory(filePath);
-
         bool doesRemoteRepoExist = true;
-
         try
         {
             // Check if the repository already exists
-            var existingRepo = await client.Repository.Get(githubUserName, name);
+            var existingRepo = await client.Repository.Get(githubUserName, ideaRepoName);
 
             Console.WriteLine($"The repository {existingRepo.Name} already exists with ID {existingRepo.Id}");
         }
-        catch (NotFoundException ex)
+        catch (NotFoundException)
         {
             Console.WriteLine("The repository does not exist.");
             doesRemoteRepoExist = false;
 
             #region remote github repo setup
             // Create a new repository
-            var newRepo = new NewRepository(name)
+            var newRepo = new NewRepository(ideaRepoName)
             {
                 Private = isPrivate,
                 Description = "This is a new repository created using the .Net GitHub API Helper, OctoKit",
-                AutoInit = false // Initialize the repository with a README file
+                AutoInit = false
             };
+
             var repo = await client.Repository.Create(newRepo);
 
             Console.WriteLine($"Created repository {repo.Name} with ID {repo.Id}");
 
-            Process.Start("git", "init").WaitForExit();
-            Process.Start("git", $"remote add origin git@github.com:{githubUserName}/{name}.git").WaitForExit();
-            File.WriteAllText("README.md", $"# {name}");
-            File.AppendAllText(".gitignore", Environment.NewLine + ".DS_Store" + Environment.NewLine + "__pycache__" + Environment.NewLine + "secrets.py");
+            await Process.Start("git", "init").WaitForExitAsync();
+            await Process.Start("git", $"remote add origin git@github.com:{githubUserName}/{ideaRepoName}.git").WaitForExitAsync();
+            await File.WriteAllTextAsync("README.md", $"# {ideaRepoName}");
+            await File.AppendAllTextAsync("README.md", $"generated repo id: {repo.Id}");
             #endregion
-
         }
         catch (Exception ex)
         {
@@ -119,9 +155,9 @@ public class Program
         }
 
         // create a new file and add content to it
-        var editProcess = Process.Start(new ProcessStartInfo
+        Process.Start(new ProcessStartInfo
         {
-            FileName = "code",
+            FileName = editorToUse,
             Arguments = fullPathToFile,
             UseShellExecute = true,
             WorkingDirectory = filePath,
@@ -142,15 +178,13 @@ public class Program
         // Clean up the watcher
         watcher.Dispose();
 
-
-        Process.Start("git", "add .").WaitForExit();
-        Process.Start("git", "commit -m \"Initial Commit\"").WaitForExit();
-
+        await Process.Start("git", "add .").WaitForExitAsync();
+        await Process.Start("git", "commit -m \"Initial Commit\"").WaitForExitAsync();
         if (!doesRemoteRepoExist)
         {
-            Process.Start("git", "branch -m main").WaitForExit();
+            await Process.Start("git", "branch -m main").WaitForExitAsync();
         }
-        Process.Start("git", "push -u origin main").WaitForExit();
+        await Process.Start("git", "push -u origin main").WaitForExitAsync();
 
         #endregion
 
