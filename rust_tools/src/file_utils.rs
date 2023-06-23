b5c582d62a51727f5325d97ea29b19d0a0021fe2
Leo Zhang <leotheasianlion@gmail.com>
2023-06-22 21:11:53 -0400

    added working thirtyfour sample code for future rust projects

--- rust_tools/src/file_utils.rs
+++ rust_tools/src/file_utils.rs
@@ -1,15 +1,30 @@
 
-//use std::env;
+use std::env;
 use walkdir::WalkDir;
 
-pub fn search_file(filename: &str, dirname: &str) {
-
+pub fn search_file(filename: &str, dirname: &str) -> () {
+    
+    // no need to check current_dir for error because we know our current directory must be valid
     // let current_dir = env::current_dir().unwrap();
+
     for entry in WalkDir::new(dirname) {
-        let entry = entry.unwrap();
-        if entry.file_name() == filename {
-            println!("Found file {} at {:?}", filename, entry.path());
+        match entry {
+            Ok(result) => {
+                if result.file_name() == filename {
+                    println!("Found file {} at {:?}", filename, result.path());
+                }
+            }
+
+            Err(err) => {
+                println!("error: {}", err);
+            }
         }
+
+        // let entry = entry.unwrap();
+
+        // if entry.file_name() == filename {
+        //     println!("Found file {} at {:?}", filename, entry.path());
+        // }
     }
 }
 
