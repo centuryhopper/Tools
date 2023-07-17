04f4e6a4a7c8ebc203c59f9746e6b7456d334d76
Leo Zhang <43508492+leozhang1@users.noreply.github.com>
2023-07-16 22:17:42 -0400

    reorganized files

--- python_requests_tools/windows_tools.py
+++
@@ -1,8 +0,0 @@
-import os
-
-AUTOMATE_TEXTING_PATH = f'C:/Users/{os.getlogin()}/Documents/GitHub/automate_texting/'
-LOG_PATH = 'C:/Windows/Temp'
-BASE_DIR = f'C:/Users/{os.getlogin()}/Documents/GitHub/'
-PATH_TO_WALLPAPERS = ''
-CHROME_DATA_PATH = ''
-CHROME_BINARY_LOCATION = ''
