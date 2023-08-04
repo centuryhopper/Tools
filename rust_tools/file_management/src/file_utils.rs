775fc0ec5d87616048d51cb360af6712c14f77b7
Leo Zhang <43508492+leozhang1@users.noreply.github.com>
2023-08-04 08:41:28 -0400

    update

--- rust_tools/file_management/src/file_utils.rs
+++ rust_tools/file_management/src/file_utils.rs
@@ -1,10 +1,9 @@
-use walkdir::WalkDir;
 use std::fs::{self, File};
-use std::io::{self, BufRead, BufReader, Write, ErrorKind};
+use std::io::{self, BufRead, BufReader, ErrorKind, Write};
 use std::path::Path;
+use walkdir::WalkDir;
 
-pub fn search_file(filename: &str, dirname: &str) -> ()
-{
+pub fn search_file(filename: &str, dirname: &str) -> () {
     // no need to check current_dir for error because we know our current directory must be valid
     // let current_dir = env::current_dir().unwrap();
 
@@ -23,11 +22,13 @@ pub fn search_file(filename: &str, dirname: &str) -> ()
     }
 }
 
-pub fn organize_files_into_folders(path: &str, extensions_to_skip: &[&str], only_target_these_extensions: &[&str]) -> io::Result<String> {
-
+pub fn organize_files_into_folders(
+    path: &str,
+    extensions_to_skip: &[&str],
+    only_target_these_extensions: &[&str],
+) -> io::Result<String> {
     let path = Path::new(path);
-    if !path.exists()
-    {
+    if !path.exists() {
         let msg = format!("path you provided: {:?} doesn't exist!", path.as_os_str());
         return Err(std::io::Error::new(ErrorKind::Other, msg));
     }
@@ -37,19 +38,25 @@ pub fn organize_files_into_folders(path: &str, extensions_to_skip: &[&str], only
     for file in files {
         let file = file?;
         let file_path = file.path();
-        
+
         if !file_path.is_file() {
             continue;
         }
-        
+
         let file_name = file_path.file_name().unwrap().to_str().unwrap().to_owned();
-        
-        if !extensions_to_skip.is_empty() && extensions_to_skip.iter().any(|&ext_to_skip| file_name.ends_with(ext_to_skip))
+
+        if !extensions_to_skip.is_empty()
+            && extensions_to_skip
+                .iter()
+                .any(|&ext_to_skip| file_name.ends_with(ext_to_skip))
         {
             continue;
         }
-        
-        if !only_target_these_extensions.is_empty() && only_target_these_extensions.iter().all(|&target_ext| !file_name.ends_with(target_ext))
+
+        if !only_target_these_extensions.is_empty()
+            && only_target_these_extensions
+                .iter()
+                .all(|&target_ext| !file_name.ends_with(target_ext))
         {
             continue;
         }
@@ -58,16 +65,23 @@ pub fn organize_files_into_folders(path: &str, extensions_to_skip: &[&str], only
         let parts: Vec<&str> = file_name.rsplitn(2, ".").collect();
         println!("parts: {:?}", parts);
         if let [ext, _] = parts.as_slice() {
-
             let destination_folder = path.join(ext);
             let destination_file_path = destination_folder.join(&file_name);
 
-            println!("{:?}", vec![destination_folder.as_os_str(), destination_file_path.as_os_str()]);
+            println!(
+                "{:?}",
+                vec![
+                    destination_folder.as_os_str(),
+                    destination_file_path.as_os_str()
+                ]
+            );
             println!();
 
-            if !destination_folder.exists()
-            {
-                println!("{} does not exist, so making a directory for it now", destination_folder.display());
+            if !destination_folder.exists() {
+                println!(
+                    "{} does not exist, so making a directory for it now",
+                    destination_folder.display()
+                );
                 fs::create_dir(&destination_folder)?;
             }
 
@@ -94,6 +108,13 @@ pub fn copy_over_file(source_file: &str, destination_file: &str) -> io::Result<(
             return Ok(());
         }
     } else {
+        if Path::new(source_file).is_dir() || Path::new(destination_file).is_dir() {
+            println!(
+                "Please make sure both your source and destination paths are files and not folders"
+            );
+            return Ok(());
+        }
+
         // Create the destination file
         // fs::File::create(destination_file)?;
         fs::create_dir_all(Path::new(destination_file).parent().unwrap())?;
@@ -125,5 +146,3 @@ fn files_are_identical(file1: &str, file2: &str) -> io::Result<bool> {
 
     Ok(contents1 == contents2)
 }
-
-
