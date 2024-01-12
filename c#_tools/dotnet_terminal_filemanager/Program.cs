1f5ce64143a4e38ef45bdc7efc76191700f3a078
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2024-01-12 18:58:33 -0500

    using python updated version of fetching nasa pic and created a simple terminal file navigator in dotnet core

---
+++ c#_tools/dotnet_terminal_filemanager/Program.cs
@@ -0,0 +1,165 @@
+﻿using Terminal.Gui;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Init();
+
+        // Set the background color to black
+        Application.Top.ColorScheme = new ColorScheme
+        {
+            Normal = Terminal.Gui.Attribute.Make(Color.White, Color.Black),
+            Focus = Terminal.Gui.Attribute.Make(Color.White, Color.Black),
+            HotNormal = Terminal.Gui.Attribute.Make(Color.White, Color.Black),
+            HotFocus = Terminal.Gui.Attribute.Make(Color.White, Color.Black),
+            Disabled = Terminal.Gui.Attribute.Make(Color.White, Color.Black),
+        };
+
+        var top = Application.Top;
+
+        var win = new Window("File Manager (Ctrl+Q to quit)")
+        {
+            X = 0,
+            Y = 0,
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+
+        var fileListView = new ListView()
+        {
+            X = 0,
+            Y = 0,
+            Width = Dim.Fill(),
+            Height = Dim.Fill() - 1,
+            AllowsMarking = false
+        };
+
+        var fileSystem = new FileSystem();
+
+        void RefreshContent()
+        {
+            // fileSystem.GetFiles().ToList().ForEach(f => System.Console.WriteLine(f));
+            fileListView.SetSource(fileSystem.GetFiles());
+        }
+
+        var statusBar = new StatusBar(new StatusItem[] {
+            new StatusItem(Key.F5, "~F5~ Refresh", () => {
+                System.Console.WriteLine("refreshing");
+                RefreshContent();
+            }),
+            // new StatusItem(Key.Q, "~Q~ Quit", () => { 
+            //     System.Console.WriteLine("stopping app");
+            //     top.Running = false;
+            //     Application.RequestStop(top: top);
+            // }),
+        });
+
+        var frame = new FrameView("Files")
+        {
+            X = 0,
+            Y = 1,
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+
+        fileListView.OpenSelectedItem += (e) =>
+        {
+            var selectedFile = fileSystem.GetFiles()[fileListView.SelectedItem];
+
+            if (File.Exists(selectedFile))
+            {
+                var fileInfo = new FileInfo(selectedFile);
+                MessageBox.Query("File Information", $"Name: {fileInfo.Name}\nSize: {fileInfo.Length} bytes", "OK");
+            }
+            else if (Directory.Exists(selectedFile))
+            {
+                fileSystem.ChangeDirectory(selectedFile);
+                RefreshContent();
+            }
+        };
+
+        bool isCursorRightPressed = false;
+        bool isLeftArrowPressed = false; 
+
+        // DO NOT USE ENTER KEY AS IT IS DEFAULTED TO SELECTION IN THIS APP
+        // DOING SO CAUSES WEIRD BEHAVIOR
+        fileListView.KeyUp += (e) =>
+        {
+            if (e.KeyEvent.Key == Key.CursorRight)
+            {
+                isCursorRightPressed = false;
+            }
+            else if (e.KeyEvent.Key == Key.CursorLeft)
+            {
+                isLeftArrowPressed = false;
+            }
+        };
+        
+        // Handle the left arrow key to move back to the parent directory
+        fileListView.KeyDown += (e) =>
+        {
+            if (e.KeyEvent.Key == Key.CursorRight && !isCursorRightPressed)
+            {
+                isCursorRightPressed = true;
+                if (fileSystem.GetFiles().Any())
+                {
+                    // System.Console.WriteLine(fileListView.SelectedItem);
+                    var selectedFile = fileSystem.GetFiles()[fileListView.SelectedItem];
+
+                    if (Directory.Exists(selectedFile))
+                    {
+                        System.Console.WriteLine("most recently visited directory: " + selectedFile);
+                        fileSystem.ChangeDirectory(selectedFile);
+                        RefreshContent();
+                    }
+                }
+            }
+            else if (e.KeyEvent.Key == Key.CursorLeft && !isLeftArrowPressed)
+            {
+                isLeftArrowPressed = true;
+                var parentDirectory = Directory.GetParent(fileSystem.GetCurrentDirectory())?.FullName;
+                // System.Console.WriteLine($"parentDirectory: {parentDirectory}");
+                if (parentDirectory != null)
+                {
+                    System.Console.WriteLine("most recently visited directory: " + parentDirectory);
+                    fileSystem.ChangeDirectory(parentDirectory);
+                    RefreshContent();
+                }
+            }
+        };
+
+        frame.Add(fileListView);
+        win.Add(frame);
+        top.Add(win, statusBar);
+        RefreshContent();
+
+        Application.Run();
+    }
+}
+
+public class FileSystem
+{
+    private string currentDirectory;
+
+    public FileSystem()
+    {
+        currentDirectory = Directory.GetCurrentDirectory();
+    }
+
+    public string GetCurrentDirectory()
+    {
+        return currentDirectory;
+    }
+
+    public string[] GetFiles()
+    {
+        return Directory.GetFileSystemEntries(currentDirectory);
+    }
+
+    public void ChangeDirectory(string newDirectory)
+    {
+        currentDirectory = newDirectory;
+    }
+}
---
+++ python_tools/fetch_nasa_pic/get_daily_pic.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python3
+
+import os
+import requests
+import platform
+from datetime import datetime
+
+
+def download_pic_of_the_day():
+    PIC_COLLECTION_NAME = "nasa_pic_collection"
+    pic_collection_path = os.path.join(os.getcwd(), PIC_COLLECTION_NAME)
+    today_string = datetime.now().strftime("%Y-%m-%d")
+    today_folder_name = os.path.join(pic_collection_path, today_string)
+
+    if not os.path.exists(pic_collection_path):
+        os.makedirs(pic_collection_path)
+
+    if os.path.exists(today_folder_name):
+        print("already gathered today's image")
+        return
+
+    os.makedirs(today_folder_name)
+
+    url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+    response = requests.get(url)
+
+    if not response.status_code == 200:
+        raise Exception(f"Error: {response.status_code}")
+
+    json_output = response.json()
+    # print(json_output)
+    
+    date,explanation,hdurl,media_type,service_version,title,url,copyright = json_output['date'],json_output['explanation'],json_output['hdurl'],json_output['media_type'],json_output['service_version'],json_output['title'],json_output['url'],json_output['copyright']
+
+    filename = os.path.join(today_folder_name, f"{today_string}.txt")
+    with open(filename, 'w') as file:
+        file.write("Date: {}\n\nExplanation: {}\n\nHdurl: {}\n\nMedia_type: {}\n\nService_version: {}\n\nTitle: {}\n\nUrl: {}\n\nCopyright: {}".format(date
+                        ,explanation
+                        ,hdurl
+                        ,media_type
+                        ,service_version
+                        ,title
+                        ,url
+                        ,copyright))
+
+    image_url = json_output['hdurl']
+    image_response = requests.get(image_url)
+
+    if image_response.status_code == 200:
+        pic_filename = f"nasa_pic_{today_string}.jpg"
+        with open(os.path.join(today_folder_name, pic_filename), 'wb') as file:
+            file.write(image_response.content)
+
+if __name__ == "__main__":
+    file_directory = "C:\\Users\\{}\\Documents\\GitHub\\Tools\\python_tools\\fetch_nasa_pic".format(os.environ.get("USERNAME")) if platform.system() == 'Windows' else ("/home/{}/Documents/GitHub/Tools/python_tools/fetch_nasa_pic/".format(os.environ.get("USER")) if platform.system() == 'Linux' else '')
+
+    if not os.path.exists(file_directory):
+        print(f'"{file_directory}" does not exist')
+    else:
+        os.chdir(file_directory)
+        download_pic_of_the_day()
+
+
+
