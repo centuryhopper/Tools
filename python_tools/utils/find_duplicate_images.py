8d3f46aca3fa04a6ace93f1f58875d04c2842ea7
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2024-08-12 20:47:08 -0400

    update

--- python_tools/utils/find_duplicate_images.py
+++ python_tools/utils/find_duplicate_images.py
@@ -3,9 +3,14 @@
 
 import os
 from PIL import Image, ImageStat
+import time
 
-image_folder = input('enter your full image directory path: ')
-image_files = [_ for _ in os.listdir(image_folder) if _.endswith('jpg') or _.endswith('png')]
+
+start = time.perf_counter()
+image_folder = input("enter your full image directory path: ")
+image_files = [
+    _ for _ in os.listdir(image_folder) if _.endswith("jpg") or _.endswith("png")
+]
 
 duplicate_files = []
 
@@ -18,9 +23,21 @@ for file_org in image_files:
             if file_check != file_org:
                 image_check = Image.open(os.path.join(image_folder, file_check))
                 pix_mean2 = ImageStat.Stat(image_check).mean
-                
+
                 if pix_mean1 == pix_mean2:
                     duplicate_files.append(file_org)
                     duplicate_files.append(file_check)
-
+        print(duplicate_files)
+end = time.perf_counter()
+print(f"total execution time: {(end - start):.1f} seconds")
 print(duplicate_files)
+
+
+"""
+./test/ took 2242.2 seconds 
+
+duplicates found:
+
+['wx_camera_1508721265523.jpg', 'wx_camera_1508721265523(1).jpg', 'mmexport1471113634580.jpg', 'mmexport1471113634580(1).jpg', 'mmexport1471113631382(1).jpg', 'mmexport1471113631382.jpg', 'IMG_20171101_203731.jpg', 'IMG_20171101_203731(1).jpg', 'mmexport1471229597243.jpg', 'mmexport1471229597243(1).jpg', 'mmexport1471229524740(1).jpg', 'mmexport1471229524740.jpg']
+
+"""
