f5459e7d9fdc0f39fb3a05b381ddbca7f002143b
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2025-02-19 00:00:35 -0500

    working on a password manager cli app in rust

--- rust_tools/cli_apps/password_manager_viewer/src/main.rs
+++ rust_tools/cli_apps/password_manager_viewer/src/main.rs
@@ -126,30 +126,6 @@ impl TableColors {
     }
 }
 
-struct Data {
-    name: String,
-    address: String,
-    email: String,
-}
-
-impl Data {
-    const fn ref_array(&self) -> [&String; 3] {
-        [&self.name, &self.address, &self.email]
-    }
-
-    fn name(&self) -> &str {
-        &self.name
-    }
-
-    fn address(&self) -> &str {
-        &self.address
-    }
-
-    fn email(&self) -> &str {
-        &self.email
-    }
-}
-
 struct App {
     state: TableState,
     items: Vec<User>,
@@ -345,30 +321,6 @@ impl App {
     }
 }
 
-fn generate_fake_names() -> Vec<Data> {
-    use fakeit::{address, contact, name};
-
-    (0..20)
-        .map(|_| {
-            let name = name::full();
-            let address = format!(
-                "{}\n{}, {} {}",
-                address::street(),
-                address::city(),
-                address::state(),
-                address::zip()
-            );
-            let email = contact::email();
-
-            Data {
-                name,
-                address,
-                email,
-            }
-        })
-        .sorted_by(|a, b| a.name.cmp(&b.name))
-        .collect()
-}
 
 fn constraint_len_calculator(items: &[User]) -> (u16, u16, u16, u16) {
     let name_len = items
@@ -401,50 +353,26 @@ fn constraint_len_calculator(items: &[User]) -> (u16, u16, u16, u16) {
     (name_len as u16, id_len as u16, email_len as u16, username_len as u16)
 }
 
-// fn constraint_len_calculator(items: &[Data]) -> (u16, u16, u16) {
-//     let name_len = items
-//         .iter()
-//         .map(Data::name)
-//         .map(UnicodeWidthStr::width)
-//         .max()
-//         .unwrap_or(0);
-//     let address_len = items
-//         .iter()
-//         .map(Data::address)
-//         .flat_map(str::lines)
-//         .map(UnicodeWidthStr::width)
-//         .max()
-//         .unwrap_or(0);
-//     let email_len = items
-//         .iter()
-//         .map(Data::email)
-//         .map(UnicodeWidthStr::width)
-//         .max()
-//         .unwrap_or(0);
-
-//     #[allow(clippy::cast_possible_truncation)]
-//     (name_len as u16, address_len as u16, email_len as u16)
-// }
 
 #[cfg(test)]
 mod tests {
-    use crate::Data;
+    // use crate::Data;
 
     #[test]
     fn constraint_len_calculator() {
-        let test_data = vec![
-            Data {
-                name: "Emirhan Tala".to_string(),
-                address: "Cambridgelaan 6XX\n3584 XX Utrecht".to_string(),
-                email: "tala.emirhan@gmail.com".to_string(),
-            },
-            Data {
-                name: "thistextis26characterslong".to_string(),
-                address: "this line is 31 characters long\nbottom line is 33 characters long"
-                    .to_string(),
-                email: "thisemailis40caharacterslong@ratatui.com".to_string(),
-            },
-        ];
+        // let test_data = vec![
+        //     Data {
+        //         name: "Emirhan Tala".to_string(),
+        //         address: "Cambridgelaan 6XX\n3584 XX Utrecht".to_string(),
+        //         email: "tala.emirhan@gmail.com".to_string(),
+        //     },
+        //     Data {
+        //         name: "thistextis26characterslong".to_string(),
+        //         address: "this line is 31 characters long\nbottom line is 33 characters long"
+        //             .to_string(),
+        //         email: "thisemailis40caharacterslong@ratatui.com".to_string(),
+        //     },
+        // ];
         // let (longest_name_len, longest_address_len, longest_email_len) =
         //     crate::constraint_len_calculator(&test_data);
 
